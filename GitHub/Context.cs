c6ce8b6e92a5266d408bbfe80cdfe3882bbe8678
Арутюн <roka96@yandex.ru>
2016-01-13 14:38:40 +0300

    Migration

---
+++ GitHub/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub
+{
+    class Context : DbContext
+    {
+        public DbSet<Sport> Sports { get; set; }
+        public DbSet<Event> Events { get; set; }
+    }
+}
