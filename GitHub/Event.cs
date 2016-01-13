335118d4aba9f26fd157eb83e08e23946e70ea66
Nikita <volkoff97@yandex.ru>
2016-01-13 14:24:24 +0300

    Entities

---
+++ GitHub/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub
+{
+    class Event
+    {
+        public int Id { get; set; }
+        public string Place { get; set; }
+        public DateTime Date { get; set; }
+        public Sport Sport { get; set; }
+    }
+}
